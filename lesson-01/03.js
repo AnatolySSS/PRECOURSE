a423dc6e719e9c5b9de85bf0a8a7733af2000918
Anatoly <Anatoly.Shilyaev@gmail.com>
2024-09-20 18:44:06 +0300

    homework 01-3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,10 +1,6 @@
 let number = 1;
-//Получи в консоли 1,3,3,1 
-console.log(number); 
-number++;
-number++;
-console.log(number); 
-console.log(number); 
-number--;
-number--;
-console.log(number);
+
+console.log(number++); 
+console.log(++number); 
+console.log(number--); 
+console.log(--number);
---
+++ lesson-01/script.js
@@ -0,0 +1,6 @@
+const h1 = document.createElement('h1');
+h1.append("IT-KAMASUTRA");
+
+const root = document.getElementById('root');
+
+root.append(h1);
